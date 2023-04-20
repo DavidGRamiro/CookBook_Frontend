65fb92f8f2384f226035281f988127f4a8b3d0de
David González Ramiro <davidgramiro@gmail.com>
2023-04-20 20:10:42 +0200

    Interface en apartado de recetas. Peticiones HTTP para todas las recetas y categorias. Peticiones HTTP para busqueda por categoria e ingrediente

--- CookBook-App/src/app/recetas/services/recetas-service.service.ts
+++ CookBook-App/src/app/recetas/services/recetas-service.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Categoria, Receta } from '../interface/recetas.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecetasServiceService {
 
-  constructor() { }
+  private endPoint: string = "http://localhost:8080";
+  private common : string = "/recetas"
+
+  constructor( private _http: HttpClient) { }
+
+
+  //Petición para obtener todas las categorias disponibles
+  todasCategorias(): Observable<Categoria[]> {
+    const url = `${ this.endPoint }${ this.common }/categorias`
+    return this._http.get<Categoria[]>(url);
+  }
+
+  //Petición para obtener todas las recetas
+  todasRecetas(): Observable<Receta[]>{
+    const url = `${ this.endPoint }${ this.common }/todas`
+    return this._http.get<Receta[]>(url)
+  }
+
+  //Petición para obtener recetas a partir de una categoría introducida por parámetro.
+  recetaPorCategoria( categoria: string): Observable<Receta[]>{
+    const url = `${ this.endPoint }${ this.common }/porCategoria`
+    const params = new HttpParams().set('categoria', categoria)
+    return this._http.get<Receta[]>(url, {params})
+  }
+
+  //Peticion para buscar una receta por un ingrediente en específico.
+  recetaPorIngrediente( ingrediente: string): Observable<Receta[]>{
+    const url = `${ this.endPoint }${ this.common }/porIngrediente`
+    const params = new HttpParams().set('ingrediente', ingrediente)
+    return this._http.get<Receta[]>(url, { params})
+  }
+
 }
