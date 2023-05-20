6f8ebdb60a3b01256b6daddb5c2d8b784984f166
Ixchel <ixchellopezalegre@gmail.com>
2023-05-20 14:13:14 +0200

    Cambios en mi perfil casi terminado

---
+++ CookBook-App/src/app/usuario/components/mi-plan/mi-plan.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { UsuarioConPlan } from '../../interface/usuarioconplan.interface';
+import { Usuario } from '../../interface/usuario.interface';
+import { UsuarioService } from '../../services/usuario.service';
+
+@Component({
+  selector: 'app-mi-plan',
+  templateUrl: './mi-plan.component.html',
+  styleUrls: ['./mi-plan.component.css']
+})
+export class MiPlanComponent implements OnInit {
+  @Input() usuario!: Usuario;
+  @Input() usuarioConPlan!: UsuarioConPlan;
+  progreso: number = 0;
+
+  constructor(private _usuarioService: UsuarioService) { }
+
+  ngOnInit(): void {
+
+    this.progreso = this.calcularProgreso();
+  }
+
+  /**
+   * Método para calcular el progreso del plan del usuario.
+   * @returns el porcentaje (0-100) de progreso del plan del usuario.
+   * @example Si el usuario tiene un plan de 30 días y lleva 15 días, el progreso será del 50%.
+   */
+  calcularProgreso(): number {
+    let fechaInicio = new Date(this.usuarioConPlan.fechaInicio);
+    let fechaActual = new Date();
+    let diasTranscurridos = Math.floor((fechaActual.getTime() - fechaInicio.getTime()) / (1000 * 60 * 60 * 24));
+    let diasPlan = this.usuarioConPlan.plan.duracion;
+    let progreso = (diasTranscurridos * 100) / diasPlan;
+    progreso = Math.round(progreso);
+    return progreso;
+  }
+
+}
