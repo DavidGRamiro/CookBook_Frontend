1ad236d1be4f57f856be34769f7bdf09425f1998
anni <adarvoy@gmail.com>
2023-06-02 21:50:15 +0200

    Modificar Planificador

--- CookBook-App/src/app/usuario/usuario-routing.module.ts
+++ CookBook-App/src/app/usuario/usuario-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { LogoutComponent } from '../auth/pages/logout/logout.component';
+import { PlannerComponent } from '../shared/pages/planner/planner.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,7 @@ const routes: Routes = [
     children: [
       { path: 'perfil', component: PerfilComponent },
       { path: 'logout', component: LogoutComponent },
+      { path: 'planificador', component: PlannerComponent },
       { path: '**', redirectTo: '' }
     ]
   }
